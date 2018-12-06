2314ed42541b6d053cfbc537c121cd066a3ccd81
Nadir Nibras <32527014+nadir2115@users.noreply.github.com>
2018-12-06 03:40:07 -0800

    updated code

--- Matching.R
+++ Matching.R
@@ -7,9 +7,13 @@ cat("\014")
 # close all plots
 graphics.off() 
 
-
-
+library(tidyverse)
+library(caret)
 library(Matching)
+library(dummy)
+library(dummies)
+library(optmatch)
+library(MatchIt)
 
 # set directory
 setwd("C:/Users/nadir/Desktop/Matching/BKN 599 Advanced Data Analysis")
@@ -19,9 +23,9 @@ evalData=read.csv("00042_evalDataset_notext.csv")
 patChar= read.csv("PatientCharacteristics_withDose.csv")
 
 # variables
-thresh= 27
+threshold= 27
 replace= 1
-onehotencode= 0
+onehotencode= 1
 
 
 names(patChar)[6]<-paste("pid")  # Changing column name for merging
@@ -32,6 +36,15 @@ patChar=patChar[-c(1:5, 7:8, 10:11, 13, 23:26, 28,31, 33)]
 
 # Merge data
 dataMerged= merge(evalData,patChar)
+dataMerged$severity= ifelse(dataMerged$severity=="H", 1, 0)
+names(dataMerged)[18]<-("Hispanic")  # Changing column name for merging
+
+
+
+# Changing table to numeric
+for (i in 2:ncol(dataMerged)){
+  dataMerged[c(i)]= as.numeric(unlist(dataMerged[c(i)]))
+}
 
 # Extract visit datas
 dataVisit1= subset(dataMerged, visitnum==1)                        
@@ -42,3 +55,39 @@ temp= data.frame(dataVisit2$pid);
 names(temp)[1]<-paste("pid");
 dataVisit1= merge(dataVisit1, temp)
 rm(temp)
+
+sum(is.na(dataVisit1))
+
+
+
+# Impute missing values- is.na(dataVisit1[,i]) returns TRUE only for row index of column with NA
+for(i in 2:ncol(dataVisit1)){
+          dataVisit1[is.na(dataVisit1[,i]), i] <- median(dataVisit1[,i], na.rm = TRUE)
+}  
+
+
+if (onehotencode==1){
+  dataVisit1$visitnum=NULL
+  dataVisit1 = dummy.data.frame(dataVisit1, names=c("center"), sep="_") 
+  dataVisit1 = dummy.data.frame(dataVisit1, names=c("DEMORace"), sep="_") 
+  dataVisit1$Hispanic= ifelse(dataVisit1$Hispanic==1, 1, 0)
+  dataVisit1$DEMORace_1=ifelse(dataVisit1$DEMORace_1==1|dataVisit1$DEMORace_2==1|dataVisit1$DEMORace_3==1,1,0)
+  dataVisit1$DEMORace_2=NULL
+  dataVisit1$DEMORace_3=NULL
+  dataVisit1$DEMORace_7=NULL
+  dataVisit1$stroke_wo_hemmo=ifelse(dataVisit1$CSstrktype==1,1,0)
+  dataVisit1$CSstrktype=NULL
+  dataVisit1 = dummy.data.frame(dataVisit1, names=c("CSstrkloc"), sep="_") 
+  dataVisit1$CSstrkloc_3=NULL
+  dataVisit1$finishedcollege=ifelse(dataVisit1$DEMOedu>4,1,0)
+  dataVisit1$DEMOedu=NULL
+}
+if (onehotencode==0){
+  dataVisit1=dataVisit1[-c(2,16:24,26,28)]
+}
+
+highdose= subset(dataVisit1, dose_hours>threshold)
+lowdose= subset(dataVisit1, dose_hours<=threshold)
+
+dataVisit1$treat=ifelse(dataVisit1$dose_hours<=threshold, 1, 0)
+
